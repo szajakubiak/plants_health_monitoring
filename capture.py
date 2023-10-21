003e9ae50c59cd76e1d5f0862f12faff29b8557b
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2023-10-21 12:17:32 +0200

    Use timestamp as file name

--- capture.py
+++ capture.py
@@ -1,10 +1,19 @@
 from picamera2 import Picamera2, Preview
-import time
+from time import sleep
+from datetime import datetime
 
+
+def get_timestamp():
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%dT%H-%M-%S")
+    return timestamp
+
+
+timestamp = get_timestamp()
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start_preview(Preview.DRM)
 picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+sleep(2)
+picam2.capture_file(timestamp + ".jpg")
