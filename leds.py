89d2b397e4faa7dd9ea20a007f6e6c0092271ed8
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2023-11-05 12:12:39 +0100

    Support for NeoPixel LED strip

--- leds.py
+++ leds.py
@@ -1,8 +1,12 @@
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import DigitalOutputDevice
+import board
+import neopixel
 
 
 IRLED_PIN = 22
+NEOPIXEL_PIN = board.D18
+NEOPIXEL_COUNT = 8
 
 
 factory = PiGPIOFactory()
@@ -20,13 +24,70 @@ class Infrared_led:
         self.output.off()
 
 
+class Rgb_led:
+    def __init__(self, pin, length):
+        self.strip = neopixel.NeoPixel(pin, length)
+        self.length = length
+        self.red = 0
+        self.green = 0
+        self.blue = 0
+    
+    def set(self):
+        for led in range(self.length):
+            self.strip[led] = (self.red, self.green, self.blue)
+
+    def on(self):
+        self.red = 255
+        self.green = 255
+        self.blue = 255
+        self.set()
+
+    def off(self):
+        self.red = 0
+        self.green = 0
+        self.blue = 0
+        self.set()
+    
+    def red_on(self):
+        self.red = 255
+        self.set()
+
+    def red_off(self):
+        self.red = 0
+        self.set()
+    
+    def green_on(self):
+        self.green = 255
+        self.set()
+
+    def green_off(self):
+        self.green = 0
+        self.set()
+    
+    def blue_on(self):
+        self.blue = 255
+        self.set()
+
+    def blue_off(self):
+        self.blue = 0
+        self.set()
+
+
 if __name__ == "__main__":
     ir_led = Infrared_led(IRLED_PIN)
+    rgb_led = Rgb_led(NEOPIXEL_PIN, NEOPIXEL_COUNT)
     ir_led.off()
+    rgb_led.off()
     while True:
         ir_led.on()
         # print("Infrared LED on")
         sleep(2)
         ir_led.off()
         # print("Infrared LED off")
+        sleep(2)
+        rgb_led.on()
+        # print("RGB LED on")
+        sleep(2)
+        rgb_led.off()
+        # print("RGB LED off")
         sleep(2)
