260215ea43b3b4b8128a75033e1848f34ce33493
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2023-11-07 18:58:14 +0100

    Set LEDs and filters depending on parameters

--- test_capture.py
+++ test_capture.py
@@ -2,13 +2,14 @@ import argparse
 from picamera2 import Picamera2, Preview
 from time import sleep
 from datetime import datetime
+import leds
+import filters
+from time import sleep
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--lights", help="lights to use", type=str, default="")
 parser.add_argument("-f", "--filters", help="filters to use", type=int, default="")
-
-
 args = parser.parse_args()
 lig = args.lights
 fil = args.filters
@@ -18,6 +19,37 @@ def get_timestamp():
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%dT%H-%M-%S")
     return timestamp
+
+
+led_rgb = leds.Rgb_led(leds.NEOPIXEL_PIN, leds.NEOPIXEL_COUNT)
+led_ir = leds.Infrared_led(leds.IRLED_PIN)
+
+
+fil_ir = filters.Filter_infrared(filters.IRCUT_PIN)
+fil_vis = filters.Filter_visible(filters.SERVO_PIN)
+
+
+led_rgb.off()
+led_ir.off()
+if "r" in lig:
+    led_rgb.red_on()
+if "g" in lig:
+    led_rgb.green_on()
+if "b" in lig:
+    led_rgb.blue_on()
+if "i" in lig:
+    led_ir.on()
+
+
+fil_ir.off()
+fil_vis.off()
+if "i" in fil:
+    fil_ir.on()
+if "v" in fil:
+    fil_vis.on()
+
+
+sleep(1)
 
 
 timestamp = get_timestamp()
