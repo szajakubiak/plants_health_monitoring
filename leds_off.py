46eb6f24c32c28fc2a7b46d6ff3af937c559f6f5
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2023-12-10 22:11:14 +0100

    Use RGBW object

--- leds_off.py
+++ leds_off.py
@@ -2,7 +2,7 @@ import leds
 
 
 ir_led = leds.Infrared_led(leds.IRLED_PIN)
-rgb_led = leds.Rgb_led(leds.NEOPIXEL_PIN, leds.NEOPIXEL_COUNT)
+rgb_led = leds.Rgbw_led(leds.NEOPIXEL_PIN, leds.NEOPIXEL_COUNT)
 
 
 ir_led.off()
