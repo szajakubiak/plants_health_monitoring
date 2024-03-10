c80395d06d2b5774105da631604fbfbc38281e8a
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2024-03-10 22:29:09 +0100

    Experiment loop

--- experiment.py
+++ experiment.py
@@ -1,7 +1,20 @@
 import os
+from time import time, sleep
 
 # Settings
 IMAGES_COUNT = 10
 CAPTURE_DELAY_SEC = 15
 LIGHTS = ["r", "g", "b", "w", "i"]
 FILTERS = ["i", "v"]
+COMMAND = "sudo ~/cam/bin/python capture.py -l {lights} -f {filters}".format
+
+time_last = 0
+for i in range(IMAGES_COUNT):
+    if time() - time_last >= CAPTURE_DELAY_SEC:
+        time_last = time()
+        for light in LIGHTS:
+            for filter in FILTERS:
+                os.system(COMMAND(lights=light, filters=filter))
+                sleep(1)
+    else:
+        sleep(1)
