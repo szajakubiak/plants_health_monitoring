198a4fa92c4682ba763517f93636eb248d609425
Szymon Jakubiak <sz.a.jakubiak@gmail.com>
2024-03-12 22:43:19 +0100

    Add debug messages

--- experiment.py
+++ experiment.py
@@ -7,14 +7,20 @@ CAPTURE_DELAY_SEC = 30
 LIGHTS = ["r", "g", "b", "w", "i"]
 FILTERS = ["i", "v"]
 COMMAND = "sudo ~/cam/bin/python capture.py -l {lights} -f {filters}".format
+DEBUG = True
 
 time_last = 0
 for i in range(IMAGES_COUNT):
     if time() - time_last >= CAPTURE_DELAY_SEC:
         time_last = time()
+        if debug:
+            print("Taking image", i + 1)
         for light in LIGHTS:
             for filter in FILTERS:
-                os.system(COMMAND(lights=light, filters=filter))
+                command = COMMAND(lights=light, filters=filter)
+                if debug:
+                    print("Command:", command)
+                os.system(command)
                 sleep(1)
     else:
         sleep(1)
